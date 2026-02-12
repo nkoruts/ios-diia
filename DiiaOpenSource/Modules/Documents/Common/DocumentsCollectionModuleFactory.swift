7726fa6846e9f7e4b3ba1dd333dddfc912277a83
Nikita Koruts <nikitakoruts@gmail.com>
2026-02-12 15:12:33 +0200

    Add walletkit

--- DiiaOpenSource/Modules/Documents/Common/DocumentsCollectionModuleFactory.swift
+++ DiiaOpenSource/Modules/Documents/Common/DocumentsCollectionModuleFactory.swift
@@ -14,32 +14,23 @@ extension DocumentsCoreNetworkContext {
     }
 }
 
-class MockDocumentsLoader: DocumentsLoaderProtocol {
-    func updateIfNeeded() { }
-    func setNeedUpdates() { }
-    func removeListener(listener: DocumentsLoadingListenerProtocol) { }
-    func addListener(listener: DocumentsLoadingListenerProtocol) { }
-}
-
 // TODO: - CLEAR CONTEXT
 struct DocumentsCollectionModuleFactory {
     static func create(holder: DocumentCollectionHolderProtocol) -> DocumentsCollectionModule {
         
-        let reorderingConfig = DocumentsReorderingConfiguration(createReorderingModule: { DocumentsReorderingModule() },
-                                                                documentsReorderingService: DocumentReorderingService.shared)
-        return  .init(context: .init(network: .create(),
-                                     documentsLoader: MockDocumentsLoader(),
-                                     docProvider: DocumentsProcessor(),
-                                     documentsStackRouterCreate: {
-                                        DocumentsStackRouter(docType: $0, docProvider: DocumentsProcessor())
-                                     },
-                                     actionFabricAllowedCodes: [DocType.driverLicense.docCode],
-                                     documentsReorderingConfiguration: reorderingConfig,
-                                     pushNotificationsSharingSubject: PassthroughSubject<Void, Never>(),
-                                     addDocumentsActionProvider: AddDocumentsActionProvider(),
-                                     imageNameProvider: DSImageNameResolver.instance,
-                                     screenBrightnessService: ScreenBrightnessHelper.shared),
-                      holder: holder
+        let reorderingConfig = DocumentsReorderingConfiguration(
+            createReorderingModule: { DocumentsReorderingModule() },
+            documentsReorderingService: DocumentReorderingService.shared
+        )
+    
+        return  .init(context: .init(
+            docProvider: DocumentsProcessor(),
+            documentsStackRouterCreate: {
+                DocumentsStackRouter(docType: $0, docProvider: DocumentsProcessor())
+            },
+            documentsReorderingConfiguration: reorderingConfig,
+            imageNameProvider: DSImageNameResolver.instance,
+            screenBrightnessService: ScreenBrightnessHelper.shared)
         )
     }
 }
